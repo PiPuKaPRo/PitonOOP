020a8ed4a6fb465f80e5db5a46bb9120dd2e9325
Evgeniy Sakov <91192469+PiPuKaPRo@users.noreply.github.com>
2023-02-28 21:47:54 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,31 +1,49 @@
+import numpy as np
+
+
 def readArrFromFile():
-    f = open('input.txt')
-    array = [int(i) for i in f.readline().split()]
+    f = open('input.txt', encoding="utf-8")
+    array = [line.replace("\n", "")
+             for line in f]
     f.close()
+
     return array
+
 
 def writeAnswerToFile(array):
     f = open("output.txt", 'w')
-    f.write(str((array)))
-
-def arrayToTuple(array):
-    return tuple(array)
+    f.write(str(array))
 
 
-def findD(a1, a2, i, j):
-    return ((a2 - a1)/(j-i))
+def task(array):
+    sentence = array[0]
+    roots = array[1:]
+    words = [word for word in sentence.split(" ")]
+
+    for k in range(len(words)):  # Проходимся по словам
+        word = words[k]  # Берем слово
+        for root in roots:  # Проходимся по корням
+            if len(word) >= len(root):
+                diff = len(word) - len(root)
+                for i in range(diff + 1):  # Проходимся по частям слова по длине корня
+                    word_root = word[i:len(word) - (diff - i)].lower()
+                    root_exc = False  # Булеана, которая проверяет одна ли ошибка в корне слова
+                    for j in range(len(word_root)): # проход по взятым буквам слова
+                        if word_root[j] != root[j]:
+                            if root_exc is True:
+                                break
+                            else:
+                                root_exc = True
+                        if root_exc is True and j == len(word_root) - 1: # если взяли дошли до конца взятых букв ка в корне и одна ошибка
+                            title = False
+                            if words[k].istitle():
+                                title = True
+                            words[k] = word[:i] + root + word[len(word_root) - i:]
+                            if title is True:
+                                words[k] = words[k].title()
+    return words
 
 
-def main():
-    arr = readArrFromFile()
-    tup = arrayToTuple(arr)
-    dictt = { }
-    for i in range(len(tup) - 1):
-        for j in range(1, len(tup)):
-
-    return 0
-
-
-
-print("Press F to start programm:")
-main()
+if __name__ == '__main__':
+    writeAnswerToFile(np.array_str(np.array(task(readArrFromFile())))
+                      .replace('[', '').replace(']', '').replace('\'', ''))
